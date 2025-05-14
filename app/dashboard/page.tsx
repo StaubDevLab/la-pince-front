c925841a0ba7993e2e918c11f52c0048676fd5af
StaubDevLab <guillaume@staub.pro>
2025-05-14 15:27:09 +0200

    🎨Style : Header start

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/header'
+
+
+export default function Dashboard() {
+    return (
+        <div className={"min-h-screen bg-background p-12 font-inter"}>
+            <Header/>
+        </div>
+    )
+}
+
