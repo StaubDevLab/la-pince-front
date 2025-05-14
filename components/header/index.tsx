df157d28e0675ff427e8151134c8e5ff57bdfe14
StaubDevLab <guillaume@staub.pro>
2025-05-14 15:39:48 +0200

    🎨 Style : Add Header component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { BellIcon } from 'lucide-react'
+import { BellIcon, CircleUser, LogOut, Settings } from 'lucide-react'
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { DropdownMenu, DropdownMenuContent,
+    DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 export default function Header() {
     return (
         <header className="flex items-center justify-between p-4 border-b dark:text-white">
@@ -9,8 +11,8 @@ export default function Header() {
                 <div className="w-10 h-10">
                     <Image src="/la-pince-logo-color.png" alt="Logo de l'application La pince qui représente un crabe à lunettes" width={100} height={100} />
                 </div>
-                <h1 className="text-2xl font-medium">
-                    Bienvenue, <span className="font-bold">John Doe</span> !
+                <h1 className="text-3xl font-medium">
+                    Bienvenue, <span className="font-bold">Bob L'éponge 🧽</span> !
                 </h1>
             </div>
             <div className="flex items-center gap-4">
@@ -24,10 +26,23 @@ export default function Header() {
                     <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full"></span>
                 </button>
                 <div className="w-8 h-8 rounded-full bg-[#f97316] flex items-center justify-center text-white cursor-pointer">
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Mon compte</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem><CircleUser />Profile</DropdownMenuItem>
+                            <DropdownMenuItem><Settings />Paramètres</DropdownMenuItem>
+                            <DropdownMenuItem><LogOut />Déconnexion</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+
 
                 </div>
             </div>
