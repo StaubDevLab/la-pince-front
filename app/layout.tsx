01851bf48ae4b889222d0d71b3abf89bf2da38e3
StaubDevLab <guillaume@staub.pro>
2025-05-14 14:56:45 +0200

    🎉Init prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,25 @@
-import type { Metadata } from "next";
-import {  Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-
+    variable: '--font-inter',
+    subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "La Pince",
-  description: "Application de gestion de budget",
-};
+    title: 'La Pince',
+    description: 'Application de gestion de budget',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="fr">
-      <body
-        className={`${inter.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="fr">
+            <body className={`${inter.variable} antialiased`}>{children}</body>
+        </html>
+    )
 }
