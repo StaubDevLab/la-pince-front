1b086c7acf3b72deeb945321d9e89245a90ef774
StaubDevLab <guillaume@staub.pro>
2025-05-15 15:50:16 +0200

    🎨Style : add chart monthly component with fake props

--- lib/fake-props.tsx
+++ lib/fake-props.tsx
@@ -1,65 +1,115 @@
 import { Apple, Bike, Bus, HeartPulse, House, Tv } from 'lucide-react'
 import type React from 'react'
 
-export const categories =  [
+export const categories = [
     {
-        name: "Logement",
+        name: 'Logement',
         value: 450,
-        fill: "#FF7A45",
+        fill: '#F97316',
         icon: (
             <div className="bg-[#FF7A45] text-white p-1 rounded-md">
                 <House />
-                </div>
+            </div>
         ),
     },
     {
-        name: "Alimentation",
+        name: 'Alimentation',
         value: 250,
-        fill: "#8884d8",
+        fill: '#8884d8',
         icon: (
             <div className="bg-[#8884d8] text-white p-1 rounded-md">
                 <Apple />
-                </div>
+            </div>
         ),
     },
     {
-        name: "Loisirs",
+        name: 'Loisirs',
         value: 120,
-        fill: "#FF5C8D",
+        fill: '#FF5C8D',
         icon: (
             <div className="bg-[#FF5C8D] text-white p-1 rounded-md">
                 <Bike />
-                </div>
+            </div>
         ),
     },
     {
-        name: "Abonnements",
+        name: 'Abonnements',
         value: 180,
-        fill: "#82E0AA",
+        fill: '#82E0AA',
         icon: (
             <div className="bg-[#82E0AA] text-white p-1 rounded-md">
                 <Tv />
-                </div>
+            </div>
         ),
     },
     {
-        name: "Transports",
+        name: 'Transports',
         value: 130,
-        fill: "#9B59B6",
+        fill: '#9B59B6',
         icon: (
             <div className="bg-[#9B59B6] text-white p-1 rounded-md">
                 <Bus />
-                </div>
+            </div>
         ),
     },
     {
-        name: "Santé",
+        name: 'Santé',
         value: 120,
-        fill: "#F7DC6F",
+        fill: '#F7DC6F',
         icon: (
             <div className="bg-[#F7DC6F] text-white p-1 rounded-md">
                 <HeartPulse />
-                </div>
+            </div>
         ),
     },
 ]
+export const expensesByCategoriesChartProps = {
+    title: 'Dépenses par catégorie',
+    categories: categories,
+    initialDateRange: {
+        from: new Date(2025, 4, 2), to: new Date(2025, 4, 18),
+    },
+}
+
+export const monthlyProps = {
+    title: 'Activité mensuelle des 6 derniers mois',
+    currentMonth: 'Juin 2025',
+    data: [
+        { month: 'Janvier', revenue: 680, expense: 1200 },
+        { month: 'Février', revenue: 950, expense: 1350 },
+        { month: 'Mars', revenue: 1350, expense: 1200 },
+        { month: 'Avril', revenue: 1380, expense: 1250 },
+        { month: 'Mai', revenue: 1400, expense: 1230 },
+        { month: 'Juin', revenue: 1420, expense: 1220 },
+    ],
+    totalRevenue: 2015.16,
+    totalExpense: 3015.16,
+}
+
+export const budgetCardProps = [{
+    title: 'Budget Courses',
+    period: '30 Jours',
+    spent: 180.12,
+    total: 450,
+    spentLabel: 'Dépenses actuels',
+    spentColor: '#F97316',
+    remainingColor: '#F0F0F0',
+},
+    {
+        title: 'Budget Logement',
+        period: '30 Jours',
+        spent: 555.12,
+        total: 450,
+        spentLabel: 'Dépenses actuels',
+        spentColor: '#F97316',
+        remainingColor: '#F0F0F0',
+    },
+    {
+        title: 'Budget Abonnements',
+        period: '30 Jours',
+        spent: 35.12,
+        total: 50,
+        spentLabel: 'Dépenses actuels',
+        spentColor: '#F97316',
+        remainingColor: '#F0F0F0',
+    }]
