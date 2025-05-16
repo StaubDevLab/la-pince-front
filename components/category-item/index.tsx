45d86f23c88a09dbbf53cfb5bbd5689361b04e46
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-05-16 16:24:45 +0200

    (style): can now adapt size

--- components/category-item/index.tsx
+++ components/category-item/index.tsx
@@ -2,6 +2,7 @@ import { categoryIconMap } from '@/lib/icons'
 
 type Category = {
     name: string
+    iconSize?: number
 }
 
 export const CategoryItem = ({ category }: { category: Category }) => {
@@ -9,7 +10,7 @@ export const CategoryItem = ({ category }: { category: Category }) => {
 
     return (
         <div className="flex items-center gap-2">
-            {Icon && <Icon size={20} />}
+            {Icon && <Icon size={category.iconSize ? category.iconSize : 20} />}
             <span>{category.name}</span>
         </div>
     )
