f8bea6235f0c508488bcdc43346187abfec4dd53
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-05-16 16:02:28 +0200

    (format): remove semicolons

--- components/category-item/index.tsx
+++ components/category-item/index.tsx
@@ -1,16 +1,16 @@
-import { categoryIconMap } from '@/lib/icons';
+import { categoryIconMap } from '@/lib/icons'
 
 type Category = {
-    name: string;
-};
+    name: string
+}
 
 export const CategoryItem = ({ category }: { category: Category }) => {
-    const Icon = categoryIconMap[category.name.toLowerCase()] || categoryIconMap['default'];
+    const Icon = categoryIconMap[category.name.toLowerCase()] || categoryIconMap['default']
 
     return (
         <div className="flex items-center gap-2">
             {Icon && <Icon size={20} />}
             <span>{category.name}</span>
         </div>
-    );
-};
+    )
+}
