92ce41f01d3c090f41a2b487260b35b34729e74c
StaubDevLab <guillaume@staub.pro>
2025-06-03 15:43:19 +0200

    ✨ Feature : add footer element and display on all pages except inscription and connexion

--- components/client-layout/index.tsx
+++ components/client-layout/index.tsx
@@ -3,16 +3,19 @@ import React from 'react'
 import { usePathname } from 'next/navigation'
 import Header from '@/components/header'
 import { useSession } from 'next-auth/react'
+import Footer from '@/components/footer'
 
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname()
     const showHeader = pathname !== '/connexion' && pathname !== '/inscription'
+    const showFooter = pathname !== '/connexion' && pathname !== '/inscription'
     const { data: session } = useSession()
     return (
-        <>
+        <main className={"flex min-h-screen flex-col"}>
             {showHeader && <Header name={session?.user?.firstName || "Invité"} />}
             {children}
-        </>
+            {showFooter && <Footer />}
+        </main>
     )
 }
