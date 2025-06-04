84224ea3ed91032d88fc1e8b1533d8fb4e087669
StaubDév <guillaume@staub.pro>
2025-06-04 22:20:45 +0200

    📝 Typo : add user type

---
+++ types/user.d.ts
@@ -0,0 +1,12 @@
+export type User = {
+    password: string;
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    createdAt: Date;
+    updatedAt: Date;
+    accountName: string;
+    accountId: string;
+    amount: float;
+}
