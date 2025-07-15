a47ea261c66e9b0e39d355e6380ed8062d89705a
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-07-15 19:32:57 +0200

    feat: move in JSX in it's own file

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,29 +1,17 @@
 import ProfileForm from '@/components/profile-form'
 import ChangePasswordForm from '@/components/change-password-form'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
-import { CreateCategoryDialog } from '@/components/category-dialog/CategoryDialog'
+import MyData from '@/components/profil-mydata'
 
 
 export default function ProfilePage() {
-
 
     return (
         <div className="p-4 flex-grow">
             <div className="max-w-4xl mx-auto">
                 {/* Profil */}
                 <ProfileForm />
-
-                <Card className='mb-6'>
-                    <CardHeader>
-                        <CardTitle className="text-lg font-medium">Mes données</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <CreateCategoryDialog isEditMod={true} triggerElement={
-                            <Button>Modifier mes catégories</Button>
-                        } />
-                    </CardContent>
-                </Card>
+                {/* Mes données */}
+                <MyData />
 
                 {/* Modifier mon mot de passe */}
                 <ChangePasswordForm />
